1af4a9a8f0c4a30dfcc51b1a18306fdff488a0f0
Deidrah <dominikastasik93@gmail.com>
2022-08-09 18:48:01 +0200

    setup, string, number, boolean, type inference, union type

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,21 @@
-console.log("Start!");
+const input1Element = document.querySelector("#input1");
+const input2Element = document.querySelector("#input2");
+const addButtonElement = document.querySelector("button");
+const add = (v1, v2) => v1 + v2;
+addButtonElement.addEventListener("click", () => {
+    const sum = add(Number(input1Element.value), Number(input2Element.value));
+    console.log(sum);
+});
+const logAge = (age) => {
+    console.log(`Hej mam ${age} lat!`);
+};
+logAge("dwadzieścia osiem");
+const buttonElement = document.querySelector("#buy");
+const calculatePrice = (originalPrice, hasDiscount) => {
+    return hasDiscount ? originalPrice * 0.8 : originalPrice;
+};
+buttonElement.addEventListener("click", () => {
+    const originalPrice = 50;
+    const hasDiscount = new URLSearchParams(window.location.search).get("discount") === "true";
+    console.log(calculatePrice(originalPrice, hasDiscount));
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,28 @@
-console.log("Start!");
+const input1Element: HTMLInputElement = document.querySelector("#input1");
+const input2Element: HTMLInputElement = document.querySelector("#input2");
+const addButtonElement = document.querySelector("button");
+
+const add = (v1: number, v2: number) => v1 + v2;
+
+addButtonElement.addEventListener("click", () => {
+  const sum = add(Number(input1Element.value), Number(input2Element.value));
+  console.log(sum);
+});
+
+const logAge = (age: number | string) => {
+  console.log(`Hej mam ${age} lat!`);
+}
+
+logAge("dwadzieścia osiem");
+
+const buttonElement: HTMLInputElement = document.querySelector("#buy");
+
+const calculatePrice = (originalPrice: number, hasDiscount: boolean) => {
+  return hasDiscount ? originalPrice*0.8 : originalPrice;
+}
+
+buttonElement.addEventListener("click", () => {
+  const originalPrice: number = 50;
+  const hasDiscount: boolean = new URLSearchParams(window.location.search).get("discount") === "true";
+  console.log(calculatePrice(originalPrice,hasDiscount));
+})
